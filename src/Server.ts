264eab2f54f2f28a88d4137dffc5bd3727410c09
Vitalii Storozh <storozh.softserve@gmail.com>
2023-08-21 11:16:51 +0300

    Access modifiers

--- src/Server.ts
+++ src/Server.ts
@@ -1,20 +1,18 @@
-
-
-
 class Server {
+  private port: number;
+  private address: string;
 
-    port: number;
-    address: string;
+  constructor(port: number, address: string) {
+    this.port = port;
+    this.address = address;
+  }
 
-    constructor(port: number, address: string) {
-        this.port = port;
-        this.address = address;
-    }
-
-    startServer(){
-        console.log(`Server started at ${this.address}:${this.port}`);
-    }
+  startServer() {
+    console.log(`Server started at ${this.address}:${this.port}`);
+  }
 }
 
 const someServer = new Server(8080, 'localhost');
 someServer.startServer();
+const somePort = (someServer as any).port;
+console.log(somePort);
